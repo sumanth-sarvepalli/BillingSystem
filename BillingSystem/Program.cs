6a94ebef92b2d5b850783312039fa0f36180430d
sumanth721 <sumanth.sarvepalli@gmail.com>
2024-06-27 18:59:39 +0530

    Initial Commit

---
+++ BillingSystem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to billing system App");
+        }
+    }
+}
